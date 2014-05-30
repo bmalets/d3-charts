c6aec00ed33e89d41911d8d52683e2984f0228e1
bmalets <b.malets@gmail.com>
2014-05-30 17:17:10 +0300

    dry code duplication

--- lib/chart_helpers/less_common/radar_chart.rb
+++ lib/chart_helpers/less_common/radar_chart.rb
@@ -2,9 +2,8 @@ module RadarChart
 
     def radar_chart( data_url, element='.radar_chart' )
       RadarChartGenerator.new.generate( element, data_url )
-      content_tag( :div, '', class: "#{element[1..-1]}_body" ) do
-        content_tag( :div, '', class: element[1..-1] )
-      end
+      radar_chart_div = content_tag( :div, '', class: element[1..-1] )
+      content_tag( :div, radar_chart_div, class: "#{element[1..-1]}_body" )
     end
 
 end
--- lib/chart_helpers/less_common/streamgraph.rb
+++ lib/chart_helpers/less_common/streamgraph.rb
@@ -2,9 +2,8 @@ module Streamgraph
 
     def streamgraph( data_url, element='.streamgraph' )
       StreamgraphGenerator.new.generate( element, data_url )
-      content_tag( :div, '', class: "#{element[1..-1]}_body" ) do
-        content_tag( :div, '', class: element[1..-1] )
-      end
+      streamgraph_div = content_tag( :div, '', class: element[1..-1] )
+      content_tag( :div, streamgraph_div, class: "#{element[1..-1]}_body" )
     end
 
 end
